fe97ede01688cdcf9a6c4789bc60e9881b9bd15d
Spencer Weiss <spweiss@seas.upenn.edu>
2017-09-05 17:35:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
 // Attribution information
+
+public class HashMap {
+  
+    public HashMap (final int size) {
+    }
+   
+    public boolean set (String key, Object value) {
+    }
+
+    public void get (String key) {
+    }
+
+    public void delete (String key) {
+    }
+
+    public float load () {
+    }
+  
+}
