e11fb48f867a98b493f16cf7554abf03630fc2b6
Spencer Weiss <spweiss@seas.upenn.edu>
2017-09-05 22:49:17 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,27 +10,38 @@ public class HashMap<E> {
         HashMap = new Object[size];
     }
     
-    public constructor (final int size) {
+    public final constructor (final int size) {
         volume = 0;
         capacity = size;
         return new HashMap(size);
     }
    
-    public boolean set (final String key, final E value) {
+    public final boolean set (final String key, final E value) {
         if (volume >=  capacity) {
             return false;
         } else {
             int hashcode = key.hashCode();
             int index = hashcode % capacity;
+            hashMap[index] = value;
+            return true;
+        }
     }
 
-    public void get (String key) {
+    public final E get (String key) {
+        int hashcode = key.hashCode();
+        int index = hashcode % capacity;
+        return hashMap[index];
     }
 
-    public void delete (String key) {
+    public final E delete (String key) {
+        int hashcode = key.hashCode();
+        int index = hashcode % capacity;
+        E target = hashMap[index];
+        hashMap[index] = null;
+        return target;
     }
 
-    public float load () {
+    public final float load () {
     }
   
 }
